5e52c2feab07cce219fea34d6c0f808f7e86dcb5
Vikor Bogdán <vikorbogdan@gmail.com>
2023-05-07 09:20:45 +0200

    refactor(front-end): Unused lines removal
    Removed commented out lines and unused import statements

--- bitek/src/hooks/useSelf.ts
+++ bitek/src/hooks/useSelf.ts
@@ -1,4 +1,4 @@
-import type { Session, User } from "next-auth";
+import type { User } from "next-auth";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
