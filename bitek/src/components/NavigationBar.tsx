242971b4af36464c0851ce709fc144e199144d99
Vikor Bogdán <vikorbogdan@gmail.com>
2023-05-11 18:13:52 +0200

    feat(front-end): Logo added to navbar
    - small-logo.svg added to assets folder
    - Logo is added to Navbar component

--- bitek/src/components/NavigationBar.tsx
+++ bitek/src/components/NavigationBar.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import type { StaticImageData } from "next/image";
+import SmallLogo from "../assets/svg/small-logo.svg";
 const NavigationBar = () => {
-  return <div className="flex">Navbar</div>;
+  return (
+    <div className="flex">
+      <div className="relative h-10 w-10">
+        <Image src={SmallLogo as StaticImageData} alt="wow" fill />
+      </div>
+    </div>
+  );
 };
 
 export default NavigationBar;
