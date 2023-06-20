b7bc93a06ec3d84df2063a605be7c0dab4589a17
Vikor Bogdán <vikorbogdan@gmail.com>
2023-06-20 17:33:43 +0200

    added images to articles

--- bitek/src/pages/article/[slug].tsx
+++ bitek/src/pages/article/[slug].tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 
@@ -8,6 +9,8 @@ const ArticlePage = () => {
     api.article.getArticleBySlug.useQuery(articleSlug);
   return (
     <div className="flex flex-col items-center gap-16 p-5">
+      {/* Add header image */}
+      <Image src={articleData?.image ?? ""} alt={""} width={500} height={300} />
       <h1 className="text-5xl">{articleData?.title}</h1>
       <h3 className="text-2xl">{articleData?.summary}</h3>
       <p className="text-lg">{articleData?.body}</p>
